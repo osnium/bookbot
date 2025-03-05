0ec95a00eece5ea7b28546408da317e44baa7aab
osnium <kai.e.kumlehn@gmail.com>
2025-03-05 22:54:35 +0100

    refactor main.py to use wildcard import from stats module and enhance text processing; add letter statistics functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import *
 
 def get_book_text(file_path):
     with open(file_path, 'r') as file:
@@ -7,8 +7,13 @@ def get_book_text(file_path):
 
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
+    book_text = book_text.lower()
+    print(book_text)
     word_count = get_num_words(book_text)
-    print(f"{word_count} words found in the document")
+    letter_stats = get_letter_stats(book_text)
+    print(letter_stats)
+
+    
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,16 @@
-
-    
 def get_num_words(text):
     return len(text.split())
+
+# returns a dictionary of how often each letter / symbol / punctuation mark appears in the text
+
+def get_letter_stats(text):
+    letter_stats = {}
+    for char in text:
+        if char in letter_stats:
+            letter_stats[char] += 1
+        else:
+            letter_stats[char] = 1
+    return letter_stats
+    
+# sorts by value in descending order
+
