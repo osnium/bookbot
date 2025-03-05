306063d90c3b5dacdfa3c1c32f4ecea6dc562926
osnium <kai.e.kumlehn@gmail.com>
2025-03-05 22:38:50 +0100

    update word count functionality to use stats module and add __pycache__ to .gitignore

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+from stats import get_num_words
+
 def get_book_text(file_path):
     with open(file_path, 'r') as file:
         return file.read()
     
-def get_word_count(text):
-    return len(text.split())
 
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
-    word_count = get_word_count(book_text)
+    word_count = get_num_words(book_text)
     print(f"{word_count} words found in the document")
 
 
